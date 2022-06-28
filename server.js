9baf3d6a0ccdf7d260011ce81c70dc778643bc04
Carly Donais <carly.donais@gmail.com>
2022-06-27 22:34:31 -0400

    added parties

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const db=mysql.createConnection(
     console.log('Connected to the election database.')
 );
 app.get('/api/candidates',(req,res)=>{
-    const sql=`SELECT * FROM candidates`;
+    const sql=`SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id=parties.id`;
 
     db.query(sql,(err,rows)=>{
         if(err){
@@ -30,7 +34,12 @@ app.get('/api/candidates',(req,res)=>{
     });
 });
 app.get('/api/candidate/:id',(req,res)=>{
-    const sql=`SELECT * FROM candidates WHERE id=?`;
+    const sql=`SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id=parties.id
+                WHERE candidates.id=?`;
     const params=[req.params.id];
 
     db.query(sql,params,(err,row)=>{
